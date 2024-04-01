b478da126bf8007ad7fc3b3750c56e50dc1335a8
Kodam-Vinay <vinay.kodam112@gmail.com>
2024-04-01 16:51:31 +0530

    added cors into the app

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { userRouter } = require("../Routes/userRouter");
 require("../db/connection");
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 8000;
 
 app.use("/api/users", userRouter);
