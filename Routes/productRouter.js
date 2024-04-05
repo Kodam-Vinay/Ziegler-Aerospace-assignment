38e7865eabbe89d7580a7ab9d32e699458d39370
Kodam-Vinay <vinay.kodam112@gmail.com>
2024-04-05 22:18:25 +0530

    added api to retrieve products

--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { addProduct } = require("../src/controllers/productController");
+const {
+  addProduct,
+  getAllProducts,
+} = require("../src/controllers/productController");
 const router = express.Router();
 
 router.post("/add-product", addProduct);
+router.post("/all-products", getAllProducts);
 
 module.exports = {
   productRouter: router,
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,7 @@
 const { ProductModel } = require("../../db/model/productModel");
 const { UserModel } = require("../../db/model/userModel");
 const { v4: uniqueId } = require("uuid");
+const jwt = require("jsonwebtoken");
 
 const addProduct = async (req, res) => {
   try {
@@ -63,4 +64,62 @@ const addProduct = async (req, res) => {
   }
 };
 
-module.exports = { addProduct };
+const retrieveAllProducts = async (req, res, Id) => {
+  try {
+    const allProducts = await ProductModel.find();
+    if (allProducts?.length > 0) {
+      return allProducts;
+    }
+  } catch (error) {
+    res.status(400).send({ message: "Something error is Happend" });
+  }
+};
+
+const getAllProducts = async (req, res) => {
+  try {
+    let jwtToken;
+
+    const { user_id, user_type } = req.body;
+    if (!user_id) return;
+
+    const checkUserExist = await UserModel.findOne({ user_id });
+    if (checkUserExist) {
+      if (checkUserExist?.user_type !== "seller") {
+        return res
+          .status(401)
+          .send({ message: "This Access is Limited to Sellers only" });
+      }
+    } else if (!checkUserExist) {
+      return res.status(401).send({ message: "User Not Exist" });
+    }
+
+    const authHeader = req.headers["authorization"];
+
+    if (authHeader) {
+      jwtToken = authHeader.split(" ")[1];
+    }
+
+    if (jwtToken && user_type === "seller") {
+      jwt.verify(jwtToken, process.env.JWT_SECRET_KEY, async (err, user) => {
+        if (err) {
+          res
+            .status(401)
+            .send({ message: "You Didn't have permission to access" });
+        } else {
+          const data = await retrieveAllProducts(req, res, user_id);
+          res.status(200).send(data);
+        }
+      });
+    } else if (jwtToken && user_type !== "seller") {
+      res
+        .status(401)
+        .send({ message: "This Access is Limited to Sellers only" });
+    } else {
+      res.status(401).send({ message: "You Didn't have permission to access" });
+    }
+  } catch (error) {
+    res.status(400).send({ message: "Something error is Happend" });
+  }
+};
+
+module.exports = { addProduct, getAllProducts };
