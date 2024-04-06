7e8edb533cc3dae9978a3a056ad1f9a20d6bef94
Kodam-Vinay <vinay.kodam112@gmail.com>
2024-04-06 18:21:21 +0530

    update

--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -13,8 +13,8 @@ router.post("/add-product", authorize, addProduct);
 router.get("/all-products", authorize, getAllProducts);
 router.post("/seller-products", authorize, retrieveAllProductsbyUserId);
 
-router.delete("/delete-product", authorize, deleteProduct);
-router.put("/update-product", authorize, updateProduct);
+router.delete("/delete-product/:product_id", authorize, deleteProduct);
+router.put("/update-product/:product_id", authorize, updateProduct);
 
 module.exports = {
   productRouter: router,
